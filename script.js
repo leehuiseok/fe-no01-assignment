6398a6c34007a1baeb069738957f62414dca8958
leehuiseok <162463130+leehuiseok@users.noreply.github.com>
2025-04-29 11:07:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch("여기에 URL을 입력")
+  // 이 URL로 웹 통신을 요청한다. 괄호 안에 다른 것이 없다면 GET!
+  .then((res) => res.json())
+  // 통신 요청을 받은 데이터는 res라는 이름으로 JSON화 한다
+  .then((data) => {
+    console.log(data); // 개발자 도구에 찍어보기
+  }); // JSON 형태로 바뀐 데이터를 data라는 이름으로 붙여 사용한다
